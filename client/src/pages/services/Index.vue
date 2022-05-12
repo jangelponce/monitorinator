c8c3b314711153d2a0877871ac84ff7700b5232c
Angel Ponce <j.angelponce.esc@gmail.com>
2022-05-11 21:15:15 -0600

    Adding axios to connect with server

--- client/src/pages/services/Index.vue
+++ client/src/pages/services/Index.vue
@@ -3,7 +3,31 @@
 </template>
 
 <script>
+// import Service from '~/backend/maenager/service'
+import axios from 'axios'
+
 export default {
-  name: 'services-index'
+  name: 'services-index',
+  data() {
+    return {
+      services: []
+    }
+  },
+  created() {
+    this.getServices()
+  },
+  methods: {
+    getServices () {
+      axios.get('http://172.29.239.236:3000/api/v1/services')
+        .then((result) => {
+          this.services = result
+        })
+        .catch((error) => {
+          alert(error)
+        })
+
+
+    }
+  }
 }
 </script>
