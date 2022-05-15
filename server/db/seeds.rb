24658f8629da18afd7f79a2884b1f524d611c14f
Angel Ponce <j.angelponce.esc@gmail.com>
2022-05-15 14:03:14 -0600

    Adding devise and user model

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+[
+  { name: "Recorrido.cl"},
+  { name: "Arendi"},
+  { name: "Wanted"},
+  { name: "Funko"}
+].each do |service_params|
+  service = Service.new(service_params)
+  if service.save
+    puts "service created: #{service.name}"
+  else
+    puts service.errors.inspect
+  end
+end
+
+service = Service.find_by_name("Funko")
+
+[
+  {
+    name: "tanjiro",
+    email: "tanjiro@funko.com",
+    password: "tanjirofunko",
+    service_id: service.id
+  },
+  {
+    name: "inosuke",
+    email: "inosuke@funko.com",
+    password: "inosukefunko",
+    service_id: service.id
+  },
+  {
+    name: "zenitsu",
+    email: "zenitsu@funko.com",
+    password: "zenitsufunko",
+    service_id: service.id
+  }
+].each do |user_params|
+  user = User.new(user_params)
+  if user.save
+    puts "user created: #{user.name}"
+  else
+    puts user.errors.inspect
+  end
+end
+
