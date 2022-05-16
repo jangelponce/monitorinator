37e2c9bd54e7ee82d287d59b85e57e97625b99ff
Angel Ponce <j.angelponce.esc@gmail.com>
2022-05-16 13:58:54 -0600

    Final touches (I hope)

--- client/src/components/DropdownItem.vue
+++ client/src/components/DropdownItem.vue
@@ -21,3 +21,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.dropdown-item {
+  cursor: pointer;
+}
+</style>
--- client/src/plugins/server/index.js
+++ client/src/plugins/server/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const config = {
-  serverUrl: process.env.VUE_APP_SERVER_URL || 'http://localhost'
+  serverUrl: process.env.VUE_APP_SERVER_URL || 'http://localhost:3000'
 }
 
 const request = axios.create({
