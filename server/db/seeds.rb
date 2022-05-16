cbb8d7bf275c02d949c581197e55766635716313
Angel Ponce <j.angelponce.esc@gmail.com>
2022-05-16 13:44:15 -0600

    Cleaning seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -7,65 +7,66 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Generates services
-# [
-#   { name: "Recorrido.cl"},
-#   { name: "Arendi"},
-#   { name: "Wanted"},
-#   { name: "Funko"}
-# ].each do |service_params|
-#   service = Service.new(service_params)
-#   if service.save
-#     puts "service created: #{service.name}"
-#   else
-#     puts service.errors.inspect
-#   end
-# end
+[
+  { name: "Funko"}
+].each do |service_params|
+  service = Service.new(service_params)
+  if service.save
+    puts "service created: #{service.name}"
+  else
+    puts service.errors.inspect
+  end
+end
 
 # # Generates users
 service = Service.find_by_name("Funko")
-# [
-#   {
-#     name: "tanjiro",
-#     email: "tanjiro@funko.com",
-#     password: "tanjirofunko",
-#     service_id: service.id
-#   },
-#   {
-#     name: "inosuke",
-#     email: "inosuke@funko.com",
-#     password: "inosukefunko",
-#     service_id: service.id
-#   },
-#   {
-#     name: "zenitsu",
-#     email: "zenitsu@funko.com",
-#     password: "zenitsufunko",
-#     service_id: service.id
-#   }
-# ].each do |user_params|
-#   user = User.new(user_params)
-#   if user.save
-#     puts "user created: #{user.name}"
-#   else
-#     puts user.errors.inspect
-#   end
-# end
-
-# Generates workshifts
-workshifts_count = 0
-(19..23).each do |hour|
-  workshift = Workshift.new(
-    {
-      timestamp: DateTime.new(2022,5,14,hour,0),
-      service_id: service.id,
-      user_id: User.pluck(:id).sample
-    }
-  )
-
-  if workshift.save
-    workshifts_count += 1
+[
+  {
+    name: "Tanjiro",
+    email: "tanjiro@funko.com",
+    password: "tanjirofunko",
+    service_id: service.id
+  },
+  {
+    name: "Inosuke",
+    email: "inosuke@funko.com",
+    password: "inosukefunko",
+    service_id: service.id
+  },
+  {
+    name: "Zenitsu",
+    email: "zenitsu@funko.com",
+    password: "zenitsufunko",
+    service_id: service.id
+  }
+].each do |user_params|
+  user = User.new(user_params)
+  if user.save
+    puts "user created: #{user.name}"
   else
-    puts workshift.errors.inspect
+    puts user.errors.inspect
+  end
+end
+
+# Generates workshifts pending
+workshifts_count = 0
+50.times.each do
+  (19..23).each do |hour|
+    day = rand(10..30)
+    workshift = Workshift.new(
+      {
+        timestamp: DateTime.new(2022,4,day,hour,0),
+        service_id: service.id,
+        user_id: User.pluck(:id).sample,
+        status: :accepted
+      }
+    )
+  
+    if workshift.save
+      workshifts_count += 1
+    else
+      puts workshift.errors.inspect
+    end
   end
 end
 
